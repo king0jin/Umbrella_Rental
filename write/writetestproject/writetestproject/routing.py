08e5d5e9f609e4b78c6826024091eaa34ad9cc41
king0jin <et0709@naver.com>
2024-12-07 11:32:23 +0900

    reUpload

--- write/writetestproject/writetestproject/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from writetestapp.consumers import UmbrellaReservationConsumer
-
-websocket_urlpatterns = [
-    path('ws/reservation/', UmbrellaReservationConsumer.as_asgi()),
-]
